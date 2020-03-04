552fb5688625f6829d7a2559e48a2d2ee50607e6
rainmaker29 <amaanrahil29@gmail.com>
2020-03-04 22:23:26 +0530

    files added

---
+++ face1.py
@@ -0,0 +1,50 @@
+import cv2
+import logging as log
+import sys
+import datetime as dt
+from time import sleep
+
+cascPath = "haarcascade_frontalface_default.xml"
+
+faceCascade = cv2.CascadeClassifier(cascPath)
+log.basicConfig(filename='webcam.log',level=log.INFO)
+
+video_capture = cv2.VideoCapture(0)
+
+anterior=0
+
+while True:
+    if not video_capture.isOpened():
+        print("Can't load cam")
+        sleep(5)
+        pass
+
+    ret,frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize = (30,30)
+
+
+    )
+
+    for(x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+
+    if anterior != len(faces):
+        anterior = len(faces)
+        log.info("faces:"+str(len(faces))+" at "+str(dt.datetime.now()))
+
+    cv2.imshow('Video',frame)
+
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        break
+
+    cv2.imshow('Video',frame)
+
+video_capture.release()
+cv2.destroyAllWindows()
